166a648221d1ca1bd0c021a4b541f15ebe3641bf
Yash Krishan <yashkmkrishan@gmail.com>
2019-01-05 19:29:39 +0530

    Update Main3Activity.java

--- app/src/main/java/com/example/yashkrishan/directory/Main3Activity.java
+++ app/src/main/java/com/example/yashkrishan/directory/Main3Activity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
 
+    //initialising the textviews
     TextView name;
     TextView mob;
     TextView loc;
@@ -20,23 +21,30 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        Intent i = getIntent();
+        Intent i = getIntent();//getting the intent from the last activity
+        
+        //getting the views from the layout file
         name = (TextView)findViewById(R.id.name2);
         mob = (TextView)findViewById(R.id.mob2);
         loc = (TextView)findViewById(R.id.loc2);
         dob = (TextView)findViewById(R.id.dob2);
         bg = (TextView)findViewById(R.id.bg2);
         id = (TextView)findViewById(R.id.id2);
+        
+        //setting the views with the data
         name.setText(i.getStringExtra("Name"));
         mob.setText(i.getStringExtra("Mob"));
         loc.setText(i.getStringExtra("Loc"));
         dob.setText(i.getStringExtra("Dob"));
         bg.setText(i.getStringExtra("Bg"));
         id.setText(i.getStringExtra("Id"));
+        
+        //initialising the back button
         btn = (Button)findViewById(R.id.back);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //going back to main2activity
                 Intent i = new Intent(Main3Activity.this, Main2Activity.class);
                 startActivity(i);
             }
@@ -45,6 +53,7 @@ public class Main3Activity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        //going back to main2activity
         Intent i = new Intent(Main3Activity.this, Main2Activity.class);
         startActivity(i);
         super.onBackPressed();
