f849a8a6afe15e3f4638ba11409a645137a1b754
Yash Krishan <yashkmkrishan@gmail.com>
2019-01-05 19:27:12 +0530

    Update Main2Activity.java

--- app/src/main/java/com/example/yashkrishan/directory/Main2Activity.java
+++ app/src/main/java/com/example/yashkrishan/directory/Main2Activity.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 public class Main2Activity extends AppCompatActivity {
 
 
+    //initialising the variables
     DatabaseReference ref;
     FirebaseDatabase database;
     ListView listView;
@@ -41,13 +42,17 @@ public class Main2Activity extends AppCompatActivity {
         person = new Person();
         listView = (ListView)findViewById(R.id.list);
         database = FirebaseDatabase.getInstance();
-        ref = database.getReference("info");
-        list = new ArrayList<>();
+        ref = database.getReference("info");//getting the database reference
+        list = new ArrayList<>();//setting up list
+        
+        //setting up the adapate for the list
         adapter = new ArrayAdapter<String>(this, R.layout.info, R.id.name1, list);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                
+                //sending data to different activity for display
                 Intent in = new Intent(Main2Activity.this, Main3Activity.class);
                 in.putExtra("Name", name);
                 in.putExtra("Mob", mob);
@@ -68,14 +73,16 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    
+                    //getting all the data
                     name = ds.child("name").getValue().toString();
                     mob = ds.child("mob").getValue().toString();
                     loc = ds.child("loc").getValue().toString();
                     dob = ds.child("dob").getValue().toString();
                     bg = ds.child("bg").getValue().toString();
                     id = ds.child("id").getValue().toString();
-                    list.add(name);
-                    Person person = new Person(bg, dob, loc, id, mob, name);
+                    list.add(name);//adding to list for the display
+                    
                 }
                 listView.setAdapter(adapter);
             }
@@ -96,6 +103,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        //switch back to the activitymain
         Intent intent = new Intent(Main2Activity.this, MainActivity.class);
         startActivity(intent);
     }
